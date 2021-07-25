b1c6f15ffecc76d9f2792568bf9b6ebdae4d7910
revanapriyandi <revanapriyandi19@gmail.com>
2021-07-25 10:19:12 +0700

    update route home

--- app/routes/HomeRoutes.js
+++ app/routes/HomeRoutes.js
@@ -3,7 +3,7 @@ const HomeController = require("../controllers/HomeController");
 var router = require("express").Router();
 var homeRouter = require("express").Router();
 
-router.get("/", HomeController.home);
+router.get("/home", HomeController.home);
 
 homeRouter.use("/", router);
 
