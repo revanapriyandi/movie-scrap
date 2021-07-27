c9494c952fbee04f1b2cdb5bd5c8b12b47619354
revanapriyandi <revanapriyandi19@gmail.com>
2021-07-28 06:16:06 +0700

    anime home

--- app/controllers/Anime/HomeController.js
+++ app/controllers/Anime/HomeController.js
@@ -12,10 +12,40 @@ const home = async (req, res, next) => {
         const response = await Axios.get(otakudeBaseUrl);
         const $ = cheerio.load(response.data);
 
+        const ongoingResponse = $(".venz")
+        
+        let ongoing = [];
+        let episode, uploaded_on, day_updated, thumb, title, link, id;
+        ongoingResponse.children()
+            .eq(0)
+            .find("ul > li")
+            .each((i, elem) => {
+                $(elem).find(".thumb > a").filter(function () {
+                    title = $(this).find(".thumbz > h2").text();
+                    thumb = $(this).find(".thumbz > img").attr("src");
+                    link = $(this).attr("href");
+                    id = link.replace(`${otakudeBaseUrl}anime/`, "");
+                });
+                uploaded_on = $(elem).find(".newnime").text();
+                episode = $(elem).find(".epz").text().replace(" ", "");
+                day_updated = $(elem).find(".epztipe").text().replace(" ", "");
+
+                ongoing.push({
+                    id,
+                    title,
+                    thumb,
+                    link,
+                    episode,
+                    uploaded_on,
+                    day_updated,
+                });
+            });
+
+
         res.send({
             status: true,
             message: "success",
-            data: null,
+            data: ongoing,
         });
 
     } catch (error) {
